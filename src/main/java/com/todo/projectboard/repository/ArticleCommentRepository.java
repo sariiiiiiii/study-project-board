470900e233c7e64c944f82ab3bdf33ca97469453
SONGSUNGGEUN <abdf13423@naver.com>
2022-12-12 20:06:03 +0900

    query 검색기능 추가

--- src/main/java/com/todo/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/todo/projectboard/repository/ArticleCommentRepository.java
@@ -1,5 +1,7 @@
 package com.todo.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.todo.projectboard.domain.ArticleComment;
 import com.todo.projectboard.domain.QArticle;
 import com.todo.projectboard.domain.QArticleComment;
@@ -14,14 +16,18 @@ import java.util.List;
 @RepositoryRestResource
 public interface ArticleCommentRepository extends
         JpaRepository<ArticleComment, Long>,
-        QuerydslPredicateExecutor<ArticleComment>, // 엔티티 필드에 대한 검색기능 추가
-        QuerydslBinderCustomizer<QArticleComment> {
-
+        QuerydslPredicateExecutor<ArticleComment>, // 엔티티 필드에 대한 모든 검색기능 추가
+        QuerydslBinderCustomizer<QArticleComment> // 선택적 검색기능으로 커스텀
+{
 
     @Override
     default void customize(QuerydslBindings bindings, QArticleComment root) {
-
+        bindings.excludeUnlistedProperties(true); // QuerydslPredicateExecutor의 의해 모든 검색기능이 열린걸 선택적으로 변경
+        bindings.including(root.content, root.createdAt, root.createdBy); // 검색기능 가능하게 할 명시
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%' 대소문자 무시
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // eq = equals는 시분초까지 동일해야되기 때문에 이 검색기능은 맞지 않음
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
     }
-
 
 }
--- src/main/java/com/todo/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/todo/projectboard/repository/ArticleRepository.java
@@ -1,16 +1,35 @@
 package com.todo.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringExpressions;
 import com.todo.projectboard.domain.Article;
 import com.todo.projectboard.domain.QArticle;
+import com.todo.projectboard.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ArticleRepository extends
         JpaRepository<Article, Long>,
         QuerydslPredicateExecutor<Article>, // 기본적으로 Article안에 모든 필드에 대한 기본 검색기능을 추가해줌
-        QuerydslBinderCustomizer<QArticle>
+        QuerydslBinderCustomizer<QArticle> // 선택적 검색기능으로 커스텀
 {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true); // QuerydslPredicateExecutor의 의해 모든 검색기능이 열린걸 선택적으로 변경
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy); // 검색기능 가능하게 할 명시
+//        bindings.bind(root.title).first(StringExpression::likeIgnoreCase); // like '${v}'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%' 대소문자 무시
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%' 대소문자 무시
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // like '%${v}%' 대소문자 무시
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // eq = equals는 시분초까지 동일해야되기 때문에 이 검색기능은 맞지 않음
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
